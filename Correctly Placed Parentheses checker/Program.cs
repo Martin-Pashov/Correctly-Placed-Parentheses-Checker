6af903fcdb33f871cfc073ea6c6c94446bfc0ec0
Martin Pashov <80414148+Martin-Pashov@users.noreply.github.com>
2021-04-06 22:11:07 +0300

    Add project files.

---
+++ Correctly Placed Parentheses checker/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Correctly_Placed_Parentheses_checker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter some text that includes parentheses: ");
+
+            string input = Console.ReadLine();
+            int count = 0;
+            int lengthOfInput = input.Length;
+
+
+            for (int i = 0; i < lengthOfInput; i++)
+            {
+                if (input[i] == '(')
+                {
+                    count++;
+                }
+                else if (input[i] == ')')
+                {
+                    count--;
+                }
+            }
+
+            if (count == 0)
+            {
+                Console.WriteLine("Properly placed parentheses!");
+            }
+            else
+            {
+                Console.WriteLine("Improperly placed parentheses!");
+            }
+        }
+    }
+}
